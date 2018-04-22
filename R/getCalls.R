e4e47c0f35320e3d6497ec095678fd9a60e22fcd
Oleg Basmanov <oleg@nexter83.ru>
2018-04-22 22:07:02 +0300

    getCalls
    new with bugs

---
+++ R/getCalls.R
@@ -0,0 +1,52 @@
+getCalls <- function(
+  token = NULL,
+  user_id = NULL,
+  date_from = NULL,
+  date_till = NULL,
+  fields = NULL){
+
+  fields <- if(is.null(fields)) {list("contact_phone_number",
+                                      "id",
+                                      "start_time")
+  }else {as.list(fields) }
+
+Calls <- content(POST("https://dataapi.uiscom.ru/v2.0",
+                          body = toJSON(
+                            list(
+                              jsonrpc="2.0",
+                              id=sample(100000:999999, 1, replace=TRUE),
+                              method="get.calls_report",
+                              params= list(
+                                access_token=token,
+                                user_id=userId,
+                                date_from = paste(date_from,"00:00:00",sep = " "),
+                                date_till = paste(date_till,"00:00:00",sep = " "),
+                                fields = fields)
+                            )
+                          )),"parsed", "application/json")
+
+totalItems <- Calls$result$metadata$total_items
+page <- ceiling(totalItems/1000)
+calls <- NULL
+for (p in 1:page) {
+  Calls <- content(POST("https://dataapi.uiscom.ru/v2.0",
+                        body = toJSON(
+                          list(
+                            jsonrpc="2.0",
+                            id=sample(100000:999999, 1, replace=TRUE),
+                            method="get.calls_report",
+                            params= list(
+                              access_token=token,
+                              user_id=userId,
+                              offset = p,
+                              date_from = paste(date_from,"00:00:00",sep = " "),
+                              date_till = paste(date_till,"00:00:00",sep = " "),
+                              fields = fields)
+                          )
+                        )),"parsed", "application/json")
+
+  CallsT <- map_df(Calls$result$data, flatten)
+  calls <- rbind(calls,CallsT)
+    }
+return(calls)
+}
